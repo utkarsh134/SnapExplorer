09e9ac438adefef1e77e67e6081701e85cec9888
utkarsh134 <utkarshpatidar450@gmail.com>
2023-12-30 00:02:31 +0530

    Js added Succesfully

--- script.js
+++ script.js
@@ -2,22 +2,56 @@
 const access_key = "xAmyoQVLqYAkbu8p2jBEb1mIzgHZ_4bSCVEdmZokpLU" ;
 
 const browse = document.getElementById('browse') ;
-const results = document.querySelector('.results') ;
+const resultBox = document.querySelector('.results') ;
+const showMore = document.getElementById('show-more-btn')
 
-browse.addEventListener('click', searchImages) ;
+
+let keywords = "" ;
+let page = 1 ;
 
 async function searchImages(){
-    const input = document.getElementById('input') ;
-    const url = `https://api.unsplash.com/search/users?page=1&query=${input.value}&client_id=${access_key}`
-    let newImg = document.createElement('img') ;
+    let input = document.getElementById('input') ;
+    keywords = input.value ;
+    if(page === 1) {
+        resultBox.innerHTML = "" ;
+    }
+    // input.value = "Yeah, It is Working"
+    let url = `https://api.unsplash.com/search/photos?page=${page}&query=${keywords}&client_id=${access_key}&per_page=12`
+    let response = await fetch(url) ;
+    let data = await response.json() ;
+    // console.log(data.results) ;
 
-    const response = await fetch(url) ;
-    const data = await response.json() ;
-    console.log(data) ;
+    (data.results).map((result)=>{
+        // console.log(element) ;
 
-    // data.results.map((ele)=>{
-    //     let newImg = document.createElement('img') ;
-    //     newImg.src = elem
-    // })
+        let newImage = document.createElement('img') ;
+        newImage.src = result.urls.small ;
+        const imageLink = document.createElement('a') ;
+        imageLink.href = result.links.html ;
+        imageLink.target = "_blank" ;
+        imageLink.appendChild(newImage) ;
+
+        resultBox.appendChild(imageLink) ;
+
+        // let photosArr = element.photos ;
+        // photosArr.forEach((arrayElement) =>{
+        //     let newImg = document.createElement('img') ;
+        //     let source = `${arrayElement.urls.small}` ;
+        //     newImg.src = `${source}` ;
+        //     results.appendChild(newImg) ;
+        // })
+    })
+    showMore.style.display = "block" ;
 
 }
+
+browse.addEventListener('click', (e)=>{
+    page = 1 ;
+    searchImages() ;
+}) ;
+
+showMore.addEventListener('click', ()=>{
+    page++ ;
+    searchImages() ;
+})
+
